10a0afc6a23acd0be78e1a0594daf66228dce1ea
claudejesus <niyigabaclaujesus@gmail.com>
2024-10-26 20:19:27 +0200

    remove coment and navigation.php

--- includes/student-navigation.php
+++ includes/student-navigation.php
@@ -3,9 +3,7 @@
                     <!-- This is for the sidebar toggle which is visible on mobile only -->
                     <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i
                             class="ti-menu ti-close"></i></a>
-                    <!-- ============================================================== -->
                     <!-- Logo -->
-                    <!-- ============================================================== -->
                     <div class="navbar-brand">
                         <!-- Logo icon -->
                         <a href="index.html">
@@ -25,39 +23,39 @@
                             </span>
                         </a>
                     </div>
-                    <!-- ============================================================== -->
+    
                     <!-- End Logo -->
-                    <!-- ============================================================== -->
-                    <!-- ============================================================== -->
+    
+    
                     <!-- Toggle which is visible on mobile only -->
-                    <!-- ============================================================== -->
+    
                     <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)"
                         data-toggle="collapse" data-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i
                             class="ti-more"></i></a>
                 </div>
-                <!-- ============================================================== -->
+
                 <!-- End Logo -->
-                <!-- ============================================================== -->
+
                 <div class="navbar-collapse collapse" id="navbarSupportedContent">
-                    <!-- ============================================================== -->
+    
                     <!-- toggle and nav items -->
-                    <!-- ============================================================== -->
+    
                     <ul class="navbar-nav float-left mr-auto ml-3 pl-1">
                         
-                        <!-- ============================================================== -->
+        
                         <!-- create new IF REQUIRED-->
-                        <!-- ============================================================== -->
+        
                         
                     </ul>
-                    <!-- ============================================================== -->
+    
                     <!-- Right side toggle and nav items -->
-                    <!-- ============================================================== -->
+    
                     <ul class="navbar-nav float-right">
                         
-                        <!-- ============================================================== -->
+        
                         <!-- User profile -->
-                        <!-- ============================================================== -->
+        
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="javascript:void(0)" data-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false">
@@ -98,9 +96,9 @@
                                 
                             </div>
                         </li>
-                        <!-- ============================================================== -->
+        
                         <!-- User profile -->
-                        <!-- ============================================================== -->
+        
                     </ul>
                 </div>
             </nav>
